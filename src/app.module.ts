60b7135cbc43617aec7622bf3b7a3bde05fcb369
ssingh <ssingh@unifocus.com>
2025-01-15 21:43:20 +0530

    Add core modules, controllers, and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { DocumentsModule } from './documents/documents.module';
+import { IngestionModule } from './ingestion/ingestion.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, DocumentsModule, IngestionModule],
   controllers: [AppController],
   providers: [AppService],
 })
